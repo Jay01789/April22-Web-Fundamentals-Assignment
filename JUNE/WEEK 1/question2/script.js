0f85d46ed75ba4c7230fd5f8339ed95c9f51cb4a
Jay01789 <102054579+Jay01789@users.noreply.github.com>
2022-06-12 20:52:42 +0530

    Add files via upload

---
+++ JUNE/WEEK 1/question2/script.js
@@ -0,0 +1,15 @@
+// for loop
+let list = ["dog", "cat", "animal", "pig", "lion"];
+for (i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
+
+// while loop
+
+i = 0;
+let listt = ["dog", "cat", "animal", "pig", "lion"];
+
+while (i < listt.length) {
+  console.log(listt[i]);
+  i++;
+}
